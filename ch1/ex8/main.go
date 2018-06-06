35e1f5eefbfca94a5a572caa212ecf574922d28a
MaxMEllon <maxmellon1994@gmail.com>
2018-06-07 01:16:34 +0900

    Add ex8

---
+++ ch1/ex8/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+func isURL(maybeURL string) bool {
+	r := regexp.MustCompile(`^http://*`)
+	return r.Match([]byte(maybeURL))
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !isURL(url) {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		resp.Body.Close()
+	}
+}
