a911720ec7eb3ebb4ae9dcb5f7111eda3163982f
MaxMEllon <maxmellon1994@gmail.com>
2018-08-22 18:34:36 +0900

    Add: ch05/ex03

---
+++ ch05/ex03/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func isScriptOrStyle(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.Data == "script" || n.Data == "style"
+}
+
+func getAllPlaneText(planeText []string, n *html.Node) []string {
+	if n == nil {
+		return planeText
+	}
+	if isScriptOrStyle(n) {
+		return planeText
+	}
+	if n.Type == html.TextNode {
+		data := strings.Replace(n.Data, "\n\n", "", -1)
+		planeText = append(planeText, data)
+	}
+	planeText = getAllPlaneText(planeText, n.FirstChild)
+	return getAllPlaneText(planeText, n.NextSibling)
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, text := range getAllPlaneText(make([]string, 0), doc) {
+		fmt.Printf("%s", text)
+	}
+}
