1872395e6e325bd0e0304e6398fd871f0943ca20
MaxMEllon <maxmellon1994@gmail.com>
2018-09-13 01:04:08 +0900

    Refactor

--- ch05/ex15/main.go
+++ ch05/ex15/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -9,22 +10,40 @@ const (
 	MAX = math.MaxInt64
 )
 
-func max(vals ...int64) int64 {
-	result := MIN
+func expectedOverOneArgs(vals []int64) error {
+	if len(vals) == 0 {
+		return fmt.Errorf("expected over one args")
+	}
+	return nil
+}
+
+func max(vals ...int64) (result int64, err error) {
+	result = MIN
+	if err := expectedOverOneArgs(vals); err != nil {
+		return 0, err
+	}
 	for _, v := range vals {
 		if result < v {
 			result = v
 		}
 	}
-	return result
+	return
 }
 
-func min(vals ...int64) int64 {
-	result := MAX
+func min(vals ...int64) (result int64, err error) {
+	if err := expectedOverOneArgs(vals); err != nil {
+		return 0, err
+	}
+	result = MAX
 	for _, v := range vals {
 		if result > v {
 			result = v
 		}
 	}
-	return result
+	return
+}
+
+func main() {
+	fmt.Println(min(0, 1, 2, 4, 5, 6))
+	fmt.Println(max(0, 1, 2, 4, 5, 6))
 }
