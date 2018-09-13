2c12968b1b2add9ce15571c9d4af1115c1814ec3
MaxMEllon <maxmellon1994@gmail.com>
2018-09-14 01:23:38 +0900

    :sparkles:

--- ch07/ex15/main.go
+++ ch07/ex15/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/maxmellon/The-Program-Language-Go/ch07/ex13/eval"
 )
@@ -19,6 +20,26 @@ func main() {
 		log.Fatal(errLog)
 		os.Exit(1)
 	}
+	vars := make(map[eval.Var]bool)
+	err = expr.Check(vars)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 	env := eval.Env{}
+	if len(vars) != 0 {
+		fmt.Printf("変数を入力\n")
+	}
+	for v := range vars {
+		fmt.Printf("%s = ", v)
+		stdin.Scan()
+		valueStr := stdin.Text()
+		value, err := strconv.ParseFloat(valueStr, 64)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		env[v] = value
+	}
 	fmt.Println(expr.Eval(env))
 }
