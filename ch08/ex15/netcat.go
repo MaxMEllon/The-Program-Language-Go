59f6bfd0a4b1294f8fc70d5b7737249275b308a2
MaxMEllon <maxmellon1994@gmail.com>
2018-10-25 03:13:42 +0900

    Add: ex15

---
+++ ch08/ex15/netcat.go
@@ -0,0 +1,47 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 227.
+
+// Netcat is a simple read/write client for TCP servers.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func init() {
+	if len(os.Args[1]) > 20 {
+		log.Fatal("username expected loss than 20 character")
+	}
+}
+
+//!+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // wait for background goroutine to finish
+}
+
+//!-
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	io.WriteString(dst, fmt.Sprintf("[%s]\n", os.Args[1]))
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
