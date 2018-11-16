1d0d8aefd8d1052427a390f72085f7a2d666b78f
MaxMEllon <maxmellon1994@gmail.com>
2018-11-16 13:27:47 +0900

    Fix

--- ch11/ex01/charcount_test.go
+++ ch11/ex01/charcount_test.go
@@ -10,30 +10,26 @@ func TestCharcount(t *testing.T) {
 	tests := []struct {
 		bytes   []byte
 		counts  map[rune]int
-		utflen  []int
 		invalid int
 	}{
 		{
 			[]byte("a"),
 			map[rune]int{'a': 1},
-			[]int{0},
 			0,
 		},
 		{
 			[]byte("あ"),
 			map[rune]int{'あ': 1},
-			[]int{0},
 			0,
 		},
 		{
 			[]byte("👨‍👩‍👧"),
 			map[rune]int{'👨': 1, '👩': 1, '👧': 1, '\u200d': 2},
-			[]int{0},
 			0,
 		},
 	}
 	for _, testCase := range tests {
-		counts, utflen, _, err := charcount(bytes.NewReader(testCase.bytes))
+		counts, _, _, err := charcount(bytes.NewReader(testCase.bytes))
 
 		if err != nil {
 			t.Errorf("%v\n", err)
@@ -46,14 +42,6 @@ func TestCharcount(t *testing.T) {
 				panic(fmt.Sprintf("count is %d, want %d\n", count, t))
 			}
 
-		}
-
-		for idx, t := range testCase.utflen {
-			act := utflen[idx]
-
-			if act != t {
-				panic(fmt.Sprintf("utflen is %d, want %d\n", t, act))
-			}
 		}
 	}
 }
